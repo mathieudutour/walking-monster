0bad2f040111379db2361c65466287f173ad0442
mathieudutour <mathieu@dutour.me>
2018-07-04 08:29:53 -0600

    Initial Commit

---
+++ The Walking MonsterTests/The_Walking_MonsterTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Walking_MonsterTests.swift
+//  The Walking MonsterTests
+//
+//  Created by Mathieu Dutour on 04/07/2018.
+//  Copyright © 2018 Mathieu Dutour. All rights reserved.
+//
+
+import XCTest
+@testable import The_Walking_Monster
+
+class The_Walking_MonsterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
